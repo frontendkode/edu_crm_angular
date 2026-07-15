a89eb5a74be8503f98536843b3d5c1ec3ff707f5
frontendkode <frontendkodedev@gmail.com>
2026-07-16 01:45:37 +0530

    init

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: true,
+    // apiUrl: 'http://10.87.99.37:8080',
+    encKey: 'WGqTO5iRgcgRXNxg',
+    apiUrl: 'http://localhost:8080',
+    // apiUrl: 'https://spring-railway-production.up.railway.app',
+
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    // apiUrl: 'http://10.87.99.37:8080',
+    // apiUrl: 'https://spring-railway-production.up.railway.app',
+    apiUrl: 'http://localhost:8080',
+};
